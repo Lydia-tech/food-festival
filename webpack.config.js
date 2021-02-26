9f68c447668807bfe5d9d8120f3ea6da6b121045
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2021-02-26 13:04:55 -0600

    module 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-// const WebpackPwaManifest = require("webpack-pwa-manifest");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 
@@ -20,52 +19,51 @@ const config = {
     rules: [
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name(file) {
-                return '[path][name].[ext]';
-              },
-              publicPath: function(url) {
-                return url.replace('../', '/assets/');
-              }
-            }
-          },
-          {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      }
-    ]
+        use: [{
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name (file) {
+              return '[path][name].[ext]'
+            },
+            publicPath: function(url) {
+                return url.replace('../', '/assets/')
+            },
+          }  
+        }, 
+        {
+          loader: 'image-webpack-loader',
+        },
+      ],
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
+        $: "jquery",
+        jQuery: "jquery"
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static'
+      analyzerMode: "static"
     }),
-  //   new WebpackPwaManifest({
-  //     publicPath: "./",
-  //     name: "Food Festival",
-  //     short_name: "Food Fest",
-  //     description: "An app that allows you to view upcoming food events.",
-  //     start_url: "../index.html",
-  //     background_color: "#01579b",
-  //     theme_color: "#ffffff",
-  //     fingerprints: false,
-  //     inject: false,
-  //     icons: [{
-  //         src: path.resolve("assets/img/icons/icon-512x512.png"),
-  //         sizes: [96, 128, 192, 256, 384, 512],
-  //         destination: path.join("assets", "icons")
-  //     }]
-  // })
-],
-mode: "development"
+    new WebpackPwaManifest({
+        publicPath: "./",
+        name: "Food Festival",
+        short_name: "Food Fest",
+        description: "An app that allows you to view upcoming food events.",
+        start_url: "../index.html",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        fingerprints: false,
+        inject: false,
+        icons: [{
+            src: path.resolve("assets/img/icons/icon-512x512.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+        }]
+    })
+  ],
+  mode: "development"
 };
 
 module.exports = config;
-
